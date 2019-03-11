72b972ce5a4eec0590203116f62b1fdc7248510d
pmckeegan <pmckeegan@gmail.com>
2019-03-11 17:55:36 +0000

    exercise 4 completed

---
+++ getAndPrintHTML3.js
@@ -0,0 +1,32 @@
+var https = require("https");
+  var chunks = "";
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+function getHTML(options, callback) {
+
+
+https.get(requestOptions, function(response){
+
+    response.setEncoding("utf8");
+    response.on("data", function(data){
+      chunks += data;
+      console.log(chunks);
+    });
+      response.on("end", function(){
+      console.log("[-*-response stream complete.-*-]");
+console.log(chunks);
+      });
+    });
+}
+function printHTML (html){
+  console.log(html);
+}
+
+
+
+getHTML();
+
