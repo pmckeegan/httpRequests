a0ab7c5330570320f58261b7782572bb812529f7
pmckeegan <pmckeegan@gmail.com>
2019-03-11 16:24:20 +0000

    exercise one complete

---
+++ httpRequests.js
@@ -0,0 +1,20 @@
+var https = require("https");
+
+
+function getAndPrintHTMLChunks(){
+  var requestOptions = {
+    host: "sytantris.github.io",
+    path: "/http-examples/step1.html"
+  };
+
+  https.get(requestOptions, function(response){
+    response.setEncoding("utf8");
+    response.on("data", function(data){
+      console.log("[------------chunk received. Length: ", data.length, "]");
+      response.on("end", function(){
+  console.log("[---response stream complete.---]");
+});  //if error (console.log("there was an error: ", error))
+});
+});
+}
+getAndPrintHTMLChunks();
